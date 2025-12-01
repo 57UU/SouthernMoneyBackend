02809347518e8434f1664411d33d0389e329a916
FrostyJ <1733799356@qq.com>
2025-12-01 19:15:25 +0800

    update contentRequest/return

--- SouthernMoneyBackend/ContentRequest.cs
+++ SouthernMoneyBackend/ContentRequest.cs
@@ -1,5 +1,8 @@
 using Database;
-
+//image part by hr
+using System;
+using System.Collections.Generic;
+//image part finished
 
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
 public class RegisterRequest
@@ -27,4 +30,65 @@ public class PostReportRequest{
     public Guid PostId { get; set; }
     public string Reason { get; set; }
 }
+
+// Image Part by HR
+//上传图片
+public class UploadImageRequest
+{
+    public byte[] File { get; set; } // 图片文件（必填，最大2MB）
+    public string ImageType { get; set; } // 图片类型（必填）
+    public string? Description { get; set; } // 图片描述（可选）
+}
+//获取图片
+public class GetImageRequest
+{
+    public Guid ImageId { get; set; } // 图片ID（必填，Guid格式）
+}
+
+// Image Part finished by hr
+// Post Part by hr (some are already written by 57u)
+// 删除帖子请求
+public class DeletePostRequest
+{
+    public Guid PostId { get; set; } // 帖子ID
+}
+// Post finished by hr
+// User by hr
+
+// 更新用户信息请求
+public class UpdateUserProfileRequest
+{
+    public string Name { get; set; } // 用户名
+    public string Email { get; set; } // 用户邮箱
+}
+
+// 修改密码请求
+public class ChangePasswordRequest
+{
+    public string CurrentPassword { get; set; } // 当前密码
+    public string NewPassword { get; set; } // 新密码
+}
+
+// 上传头像请求
+public class UploadAvatarRequest
+{
+    public byte[] File { get; set; } // 图片文件（必填，最大2MB）
+}
+
+// 充值请求
+public class TopUpRequest
+{
+    public decimal Amount { get; set; } // 充值金额
+}
+//User Part Finished by hr
+// transaction by hr 
+// 购买商品请求
+public class BuyProductRequest
+{
+    public Guid ProductId { get; set; }
+}
+
+
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
+
+
--- SouthernMoneyBackend/ContentReturn.cs
+++ SouthernMoneyBackend/ContentReturn.cs
@@ -1,8 +1,9 @@
-﻿namespace SouthernMoneyBackend;
+﻿// this is all by hr
+namespace SouthernMoneyBackend;
 
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
 
-
+//login
 public class PostDto
 {
     public Guid Id { get; set; }
@@ -23,7 +24,64 @@ public class PostUploaderDto
 {
     public long Id { get; set; }
     public string Name { get; set; }
+    public Guid Avatar { get; set; }
 }
 
+public class PostsPageDto
+{
+    public int TotalPages { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public List<PostDto> Posts { get; set; } = new();
+}
+public class PostLikeResultDto
+{
+    public int LikeCount { get; set; }
+}
+//user
+public class UserAssetDto
+{
+    public decimal Total { get; set; }
+    public decimal TodayEarn { get; set; }
+    public decimal AccumulatedEarn { get; set; }
+    public decimal EarnRate { get; set; }
+    public decimal Balance { get; set; }
+}
 
+public class UserProfileDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public Guid Avatar { get; set; }
+    public bool IsBlocked { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public UserAssetDto Asset { get; set; }
+}
+
+public class UploadAvatarResultDto
+{
+    public Guid AvatarId { get; set; }
+}
+
+//transaction
+public class PurchaseRecordDto
+{
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime PurchaseTime { get; set; }
+}
+
+public class PurchaseRecordsPageDto
+{
+    public int TotalPages { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public List<PurchaseRecordDto> Records { get; set; } = new();
+}
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
+
+// finished by hr
